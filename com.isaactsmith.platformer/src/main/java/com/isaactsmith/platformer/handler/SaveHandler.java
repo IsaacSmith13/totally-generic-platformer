06ca4420354d45f4151bc0d4f0bcb007c9a6faf3
IsaacSmith13 <Isaac13Smith@gmail.com>
2019-06-22 22:37:57 -0400

    Hid level number in save file to be less easily manipulated

--- com.isaactsmith.platformer/src/main/java/com/isaactsmith/platformer/handler/SaveHandler.java
+++ com.isaactsmith.platformer/src/main/java/com/isaactsmith/platformer/handler/SaveHandler.java
@@ -5,26 +5,37 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public abstract class SaveHandler {
-	
+
 	private static final File SAVE_FILE = new File("savefile.save");
 
 	public static void addLevel(int levelNumber) {
 		try (BufferedWriter writer = new BufferedWriter(new PrintWriter(SAVE_FILE))) {
-			writer.write("" + levelNumber);
+			for (int i = 0; i < 37; i++) {
+				writer.write((int) (Math.random() * 20) + " ");
+			}
+			writer.write(levelNumber + " ");
+			for (int i = 0; i < 13; i++) {
+				writer.write((int) (Math.random() * 20) + " ");
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		
 	}
 
 	public static int readSave() {
 		try (Scanner scanner = new Scanner(SAVE_FILE)) {
-			return scanner.nextInt();
-		} catch (FileNotFoundException e) {
+			String[] saveFileLine = scanner.nextLine().split(" ");
+			for (int i = 0; i < saveFileLine.length; i++) {
+				System.out.println(i + " " + saveFileLine[i]);
+			}
+			System.out.println(saveFileLine[37]);
+			return Integer.parseInt(saveFileLine[37]);
+		} catch (FileNotFoundException | InputMismatchException | NumberFormatException e) {
 			e.printStackTrace();
 		}
 		return 0;
